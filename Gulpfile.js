0e00ca5dbd591d2706a74ad7d63a139725d3029d
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-02-26 12:50:59 +0700

    Update Gulp and NPM package

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,57 +1,96 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
 
-var scssSrc = 'assets/scss/*.scss';
-var scssDest = 'assets/css';
-
-var cssSrc = 'assets/css/*.css';
-var cssSrcExclude = '!assets/css/*.min.css';
-var cssDest = 'assets/css';
-
-var jsSrc = 'assets/js/*.js';
-var jsSrcExclude = '!assets/js/*.min.js';
-var jsDest = 'assets/js';
+// Set assets paths.
+const paths = {
+	'css': {
+		'src': ['./assets/css/*.css', '!./assets/css/*.min.css'],
+		'dest': 'assets/css'
+	},
+	'sass': {
+		'src': ['assets/sass/*.scss'],
+		'dest': 'assets/css',
+	},
+	'js': {
+		'src': ['assets/js/*.js', '!assets/js/*.min.js'],
+		'dest': 'assets/js',
+	},
+	'php': ['./*.php', './**/*.php']
+};
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function () {
-	return gulp.src(scssSrc)
+	return gulp.src(paths.sass.src)
+		.pipe(plumber({
+			errorHandler: function (error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		}))
+		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest(scssDest));
+		.pipe(autoprefixer({
+			'browsers': ['last 2 version']
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.sass.dest))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function () {
-	return gulp.src([cssSrc, cssSrcExclude])
+	return gulp.src(paths.css.src)
 		.pipe(cleanCSS({
 			compatibility: 'ie8'
 		}))
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest(cssDest));
+		.pipe(gulp.dest(paths.css.dest));
 });
 
 // Minify custom JS
 gulp.task('minify-js', function () {
-	return gulp.src([jsSrc, jsSrcExclude])
+	return gulp.src(paths.js.src)
+		.pipe(plumber({
+			errorHandler: function (error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		}))
 		.pipe(uglify())
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest(jsDest));
+		.pipe(gulp.dest(paths.js.dest));
+});
+
+// Dev task with watch
+gulp.task('watch', function () {
+
+	// Kick off BrowserSync.
+	browserSync({
+		'open': false,             // Open project in a new tab?
+		'injectChanges': true,     // Auto inject changes instead of full reload.
+		'proxy': 'https://woo.dev',    // Use http://_s.com:3000 to use BrowserSync.
+		'watchOptions': {
+			'debounceDelay': 1000  // Wait 1 second before injecting.
+		}
+	});
+
+	gulp.watch(paths.sass.src, ['sass']);
+	gulp.watch(paths.css.src, ['minify-css']);
+	gulp.watch(paths.js.src, ['minify-js']).on('change', browserSync.reload);
 });
 
 // Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js']);
-
-// Dev task with watch
-gulp.task('watch', ['sass', 'minify-css', 'minify-js'], function () {
-	gulp.watch(scssSrc, ['sass']);
-	gulp.watch([cssSrc, cssSrcExclude], ['minify-css']);
-	gulp.watch([jsSrc, jsSrcExclude], ['minify-js']);
-});
+gulp.task('default', ['minify-css', 'minify-js']);
